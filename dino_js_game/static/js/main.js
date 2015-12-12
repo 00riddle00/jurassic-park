62bd87d50707a6b5e7d04f7aaf98359beeb6c4d5
00riddle00 <tomasgiedraitis@gmail.com>
2015-12-12 23:35:31 +0200

    Added div collision function

--- dino_js_game/static/js/main.js
+++ dino_js_game/static/js/main.js
@@ -3,6 +3,39 @@ jQuery.noConflict();
 
 (function ($) {
 
+  var dino = $('#dino');
+  var dino2 = $('#dino2');
+  var coin = $('.coin.c2');
+
+
+  function collision($div1, $div2) {
+      var x1 = $div1.offset().left;
+      var y1 = $div1.offset().top;
+      var h1 = 55
+      var w1 = 105 
+      var b1 = y1 + h1;
+      var r1 = x1 + w1;
+      var x2 = $div2.offset().left;
+      var y2 = $div2.offset().top;
+      var h2 =  65
+      var w2 = 125
+      var b2 = y2 + h2;
+      var r2 = x2 + w2;
+
+      console.log(x1, y1, h1, w1, b1, r1, x2, y2, h2, w2, b2, r2);
+
+      if (b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2) {
+        console.log('false');
+	$('#balloon').hide();
+	$('#greeting').hide();
+      } else {
+	console.log('true');
+	$('#balloon').show();
+	$('#greeting').show();
+      }
+  }
+
+
         var gold_counter = 0;
         var l_pos = 0; // leg_position
         var r_dir = 'right' // run_direction
@@ -58,26 +91,32 @@ jQuery.noConflict();
                 case 37: // left
                     $('#dino').css({marginLeft: '-=15px'});
                     change_legs_left()
+		     collision(dino, dino2);
                     break;
 
                 case 38: // up
                     $('#dino').css({marginTop: '-=15px'});
                     change_legs_up()
+		     collision(dino, dino2);
                     break;
 
                 case 39: // right
                     $('#dino').css({marginLeft: '+=15px'});
                     change_legs_right()
+		     collision(dino, dino2);
                     break;
 
                 case 40: // down
                     $('#dino').css({marginTop: '+=15px'});
                     change_legs_down()
+		     collision(dino, dino2);
                     break;
 
                 default: return;
             }
         });
+
+
 
 }(jQuery));
 
