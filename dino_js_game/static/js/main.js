9cf01dd8911b6f6e1afc455217ad48132f1aab4a
00riddle00 <tomasgiedraitis@gmail.com>
2015-12-13 20:34:01 +0200

    Added coin collecting and another dino encounter event

--- dino_js_game/static/js/main.js
+++ dino_js_game/static/js/main.js
@@ -3,10 +3,16 @@ jQuery.noConflict();
 
 (function ($) {
 
-  var dino = $('#dino');
-  var dino2 = $('#dino2');
-  var coin = $('.coin.c2');
+  var s_coin = new Audio('static/audio/coin.mp3')
+  var s_jungle = new Audio('static/audio/jungle.mp3')
+  var s_hello = new Audio('static/audio/hello.mp3')
 
+  var dino = $('#dino');
+
+  // triceratops
+  var dino2 = $('#dino2');
+
+  var gold_counter = 0;
 
   function collision($div1, $div2) {
       var x1 = $div1.offset().left;
@@ -28,15 +34,54 @@ jQuery.noConflict();
         console.log('false');
 	$('#balloon').hide();
 	$('#greeting').hide();
+        s_hello.pause();
+        s_hello.currentTime = 0;
+        $('#dino2').css("background-image", "url('/static/img/triceratops.png')");
       } else {
 	console.log('true');
+            $('#dino2').css("background-image", "url('/static/img/triceratops_hello.png')");
+        setTimeout(function(){
+            s_hello.play();
+        }, 100);
 	$('#balloon').show();
 	$('#greeting').show();
       }
   }
 
+  function ccollision($div1, $div2) {
+      var x1 = $div1.offset().left;
+      var y1 = $div1.offset().top;
+      var h1 = 55
+      var w1 = 105 
+      var b1 = y1 + h1;
+      var r1 = x1 + w1;
+      var x2 = $div2.offset().left;
+      var y2 = $div2.offset().top;
+      // get params with regex
+      var h2 =  15
+      var w2 = 15
+      var b2 = y2 + h2;
+      var r2 = x2 + w2;
 
-        var gold_counter = 0;
+      console.log(x1, y1, h1, w1, b1, r1, x2, y2, h2, w2, b2, r2);
+
+      if (b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2) {
+        console.log('no_coin');
+      } else {
+        s_coin.play();
+	console.log('coin');
+	$div2.hide();
+        gold_counter++ 
+        $('#number').html(gold_counter + '/5');
+      }
+  }
+
+
+
+
+
+
+
         var l_pos = 0; // leg_position
         var r_dir = 'right' // run_direction
 
@@ -91,30 +136,68 @@ jQuery.noConflict();
                 case 37: // left
                     $('#dino').css({marginLeft: '-=15px'});
                     change_legs_left()
-		     collision(dino, dino2);
+		    collision(dino, dino2);
+                    $('.coin').each(function () {
+                        ccollision(dino, $(this));
+                    });
                     break;
 
                 case 38: // up
                     $('#dino').css({marginTop: '-=15px'});
                     change_legs_up()
-		     collision(dino, dino2);
+		    collision(dino, dino2);
+                    $('.coin').each(function () {
+                        ccollision(dino, $(this));
+                    });
                     break;
 
                 case 39: // right
                     $('#dino').css({marginLeft: '+=15px'});
                     change_legs_right()
-		     collision(dino, dino2);
+		    collision(dino, dino2);
+                    $('.coin').each(function () {
+                        ccollision(dino, $(this));
+                    });
                     break;
 
                 case 40: // down
                     $('#dino').css({marginTop: '+=15px'});
                     change_legs_down()
-		     collision(dino, dino2);
+		    collision(dino, dino2);
+                    $('.coin').each(function () {
+                        ccollision(dino, $(this));
+                    });
                     break;
 
                 default: return;
             }
         });
+
+
+        $(document).ready(function() {
+
+            s_jungle.play()
+
+            setTimeout(function() {
+                $('#dino2').css({marginLeft: '-=15px'});
+            }, 3000);
+
+            setTimeout(function() {
+                $('#dino2').css({marginLeft: '-=15px'});
+            }, 3000);
+            setTimeout(function() {
+                $('#dino2').css({marginLeft: '-=15px'});
+            }, 3000);
+            setTimeout(function() {
+                $('#dino2').css({marginLeft: '-=15px'});
+            }, 3000);
+            setTimeout(function() {
+                $('#dino2').css({marginLeft: '-=15px'});
+            }, 3000);
+        });
+
+
+
 
 
 
